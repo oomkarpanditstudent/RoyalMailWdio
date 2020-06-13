86d494adef10544ee36c85246897f266bd241564
Oomkar Pandit <oomkar.pandit@royalmail.com>
2020-06-13 17:40:28 +0100

    adding jenkins features

--- wdio.conf.js
+++ wdio.conf.js
@@ -121,8 +121,8 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl:process.env[process.env.server]  ,  //url[process.env.server],
-  //  baseUrl:'http://www.royalmail.com',
+   // baseUrl:process.env[process.env.server]  ,  //url[process.env.server],
+    baseUrl:'http://www.royalmail.com',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
